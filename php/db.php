5abbaed802535c8400abbb7838ca7e9e5cd4ecd3
sotiriospap <sotospapas96@gmail.com>
2023-02-07 02:56:13 +0200

    Delete php directory

--- php/db.php
+++
@@ -1,7 +0,0 @@
-<?php
-// Ανοιγω συνδεση με τη βαση δεδομενων
-	  $link = mysqli_connect("127.0.0.1","ddpms22533","dpmsp@$$","ddpms22533");
-	
-	  if(mysqli_connect_error()){
-			exit ("Connect error: ".mysqli_connect_error());
-		}
